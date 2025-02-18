ee27d67b90a7d4a0b3a200097260f7d20b70c7e6
Brian Corbin <brian.william.corbin@gmail.com>
2025-02-17 18:19:04 -0800

    wip

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ParaSwift"),
+            name: "ParaSwift",
+            dependencies: ["BigInt"]),
         .testTarget(
             name: "ParaSwiftTests",
             dependencies: ["ParaSwift"]),
